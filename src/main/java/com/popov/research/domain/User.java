5703fc443a7a2197048de77fed019898e7e48b0b
Popov85 <andrey.popov85@gmail.com>
2021-02-08 08:49:49 +0200

    Adjusted User

--- src/main/java/com/popov/research/domain/User.java
+++ src/main/java/com/popov/research/domain/User.java
@@ -3,11 +3,19 @@ package com.popov.research.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
 
     private String name;
+    
+    public void doSmth() {
+    	String str = new String();
+    	Integer num = Integer.valueOf(1);
+    	log.debug("Str = {}", str);
+    }
 }
